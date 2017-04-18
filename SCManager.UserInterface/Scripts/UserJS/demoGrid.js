0e4a63f8165a1ae7b89f09cc5a7b5f905e5ce1eb
suvaneeth <suvaneeth@gmail.com>
2017-04-18 17:14:44 +0300

    combo

--- SCManager.UserInterface/Scripts/UserJS/demoGrid.js
+++ SCManager.UserInterface/Scripts/UserJS/demoGrid.js
@@ -1,9 +1,30 @@
 ﻿var DataTables = {};
 var editor;
-var totalDetailRows = 0;
-var GridData;
-var identityColumn = 'SlNo';
-var GridInputPerRow=7;
+var availableTags = [
+     "ActionScript",
+     "AppleScript",
+     "Asp",
+     "BASIC",
+     "C",
+     "C++",
+     "Clojure",
+     "COBOL",
+     "ColdFusion",
+     "Erlang",
+     "Fortran",
+     "Groovy",
+     "Haskell",
+     "Java",
+     "JavaScript",
+     "Lisp",
+     "Perl",
+     "PHP",
+     "Python",
+     "Ruby",
+     "Scala",
+     "Scheme"
+];
+
 //---------------------------------------Docuement Ready--------------------------------------------------//
 $(document).ready(function () {
     try {
@@ -15,27 +36,9 @@ $(document).ready(function () {
               order: [],
               searching: false,
               paging: false,
-              data: GetGridDate(),
-              columns: [
-                { "data": "SCCode", "defaultContent": "<i></i>" },
-                { "data": "ID", "defaultContent": "<i>0</i>" },
-                { "data": "SlNo", "defaultContent": "<i></i>" },
-                { "data": "Material", render: function (data, type, row) { return (createTextBox(data, type, row, 'Material')); } },
-                { "data": "Quantity", render: function (data, type, row) { return (createTextBox(data, type, row, 'Quantity')); }, "defaultContent": "<i></i>" },
-                { "data": "UOM", render: function (data, type, row) { return (createTextBox(data, type, row, 'UOM')); }, "defaultContent": "<i></i>" },
-                { "data": "Rate", render: function (data, type, row) { return (createTextBox(data, type, row, 'Rate')); }, "defaultContent": "<i></i>" },
-                { "data": "BasicAmount", render: function (data, type, row) { return (createTextBox(data, type, row, 'BasicAmount')); }, "defaultContent": "<i></i>" },
-                { "data": "TradeDiscount", render: function (data, type, row) { return (createTextBox(data, type, row, 'TradeDiscount')); }, "defaultContent": "<i></i>" },
-                { "data": "NetAmount", render: function (data, type, row) { return (createTextBox(data, type, row, 'NetAmount')); }, "defaultContent": "<i></i>" }
-              ],
-              columnDefs: [{ "targets": [0], "visible": false, "searchable": false }, { "targets": [1], "visible": false, "searchable": false },
-                  { "targets": [3], "width": "20%" },
-                   { className: "text-right", "targets": [6, 7, 8, 9] },
-              { className: "text-center", "targets": [2, 3, 4, 5] },
-
-
-
-              ],
+              data: GetGridData(),
+              columns: EG_Columns(),
+              columnDefs: EG_Columns_Settings(),
               keys: {
                     columns: ':not(:first-child)',
                     editor:  editor
@@ -46,75 +49,95 @@ $(document).ready(function () {
                     blurable: true
                     }
           });
-
-
        
-      
+        var a = getMaterials();        
+        EG_ComboSource('Materials',a)
     
     }catch(x){}
 
 });
 
+//-----------------------EDIT GRID DEFN-------------------------------------
+var EG_totalDetailRows = 0;
+var EG_GridData;
+var EG_SlColumn = 'SlNo';
+var EG_GridInputPerRow = 7;
 
+function EG_TableDefn() {
 
-function blankRow(count) {
-
-    var dataObj = [];
-    for (i = 0; i < count; i++) {
-
-        var tempObj = new Object();
-        tempObj.SCCode = "";
-        tempObj.ID = "";
-        tempObj.SlNo = totalDetailRows + i + 1;
-        tempObj.Material = "";
-        tempObj.Quantity = "";
-        tempObj.UOM = "";
-        tempObj.Rate = "";
-        tempObj.BasicAmount = "";
-        tempObj.TradeDiscount = "";
-        tempObj.NetAmount = "";
-        dataObj.push(tempObj);
-
-    }
-
-    totalDetailRows = totalDetailRows + count;
-    return dataObj;
-
-}
-
-function GetGridDate() {
-
-    GridData = blankRow(20);
-    return GridData;
-}
-
-function changeData(value,row,column) {
-   // debugger;
-  
-    for (i = 0; i < GridData.length; i++)
-    {        
-        if (GridData[i][identityColumn] == row)
-        {
-            GridData[i][column] = value;
-            break;
-        }
-    }
+    var tempObj = new Object();
+    tempObj.SCCode = "";
+    tempObj.ID = "";
+    tempObj.SlNo = 0;
+    tempObj.Material = "";
+    tempObj.Quantity = "";
+    tempObj.UOM = "";
+    tempObj.Rate = "";
+    tempObj.BasicAmount = "";
+    tempObj.TradeDiscount = "";
+    tempObj.NetAmount = "";
    
+    return tempObj
 }
 
-function createTextBox(data, type, row,columnname) {
-    
-    var a = row[columnname];
-    var b = row.SlNo;
-    var c = "'";
-   // debugger;
-    if (data=="" || data == null) {
-       
-        return ('<input type="textbox" class="gridTextbox" value="" onblur="changeData(this.value,' + b + ',' + c + columnname + c + ')" >  </input>');
-    } else {
-        return ('<input type="textbox" class="gridTextbox" value=' + data + ' onblur="changeData(this.value,' + b + ',' + c + columnname + c + ')" >  </input>');
-    }
-   
+function EG_Columns() {
+    var obj=[
+                { "data": "SCCode", "defaultContent": "<i></i>" },
+                { "data": "ID", "defaultContent": "<i>0</i>" },
+                { "data": "SlNo", "defaultContent": "<i></i>" },
+                { "data": "Material", render: function (data, type, row) { return (EG_createCombo(data, type, row, 'Material', 'Materials')); } },
+                { "data": "Quantity", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'Quantity')); }, "defaultContent": "<i></i>" },
+                { "data": "UOM", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'UOM')); }, "defaultContent": "<i></i>" },
+                { "data": "Rate", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'Rate')); }, "defaultContent": "<i></i>" },
+                { "data": "BasicAmount", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'BasicAmount')); }, "defaultContent": "<i></i>" },
+                { "data": "TradeDiscount", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'TradeDiscount')); }, "defaultContent": "<i></i>" },
+                { "data": "NetAmount", render: function (data, type, row) { return (EG_createTextBox(data, type, row, 'NetAmount')); }, "defaultContent": "<i></i>" }
+    ]
 
+    return obj
+
+}
+
+function EG_Columns_Settings() {
+
+    var obj = [
+        { "targets": [0], "visible": false, "searchable": false }, { "targets": [1], "visible": false, "searchable": false },
+        { "targets": [3], "width": "20%" },
+        { className: "text-right", "targets": [6, 7, 8, 9] },
+        { className: "text-center", "targets": [2, 3, 4, 5] }
+
+    ]
+
+    return obj;
+
+}
+
+//------------------------------------------------------------------
+
+
+function GetGridData() {
+
+    EG_GridData = EG_blankRow(20);
+    return EG_GridData;
+}
+
+
+function getMaterials() {
+
+    var mymats = new Array();
+    mymats[0] = 'bush';
+    mymats[1] = 'door';
+    mymats[2] = 'clamp';
+    mymats[3] = 'clip';
+    mymats[4] = 'additive';
+    mymats[5] = 'liquid';
+    mymats[6] = 'drum';
+    mymats[7] = 'lock';
+    mymats[8] = 'mat';
+    mymats[9] = 'footer';
+    mymats[10] = 'badge';
+    mymats[11] = 'plug';
+
+    return mymats;
 }
 
--- SCManager.UserInterface/Scripts/editableGrid.js
+++ SCManager.UserInterface/Scripts/editableGrid.js
@@ -15,7 +15,7 @@
                         break;
 
                     case 38: // up
-                        var index = $('.gridTextbox').index(this) - GridInputPerRow;
+                        var index = $('.gridTextbox').index(this) - EG_GridInputPerRow;
                         $('.gridTextbox').eq(index).focus();
                         break;
 
@@ -25,7 +25,7 @@
                         break;
 
                     case 40: // down
-                        var index = $('.gridTextbox').index(this) + GridInputPerRow;
+                        var index = $('.gridTextbox').index(this) + EG_GridInputPerRow;
                         $('.gridTextbox').eq(index).focus();
                         break;
 
@@ -40,3 +40,90 @@
     });
 
 });
+
+
+
+function EG_changeData(value, row, column) {
+     debugger;
+  //  EG_GridData[row-1][column] = value;
+    for (i = 0; i < EG_GridData.length; i++) {
+        if (EG_GridData[i][EG_SlColumn] == row) {
+            EG_GridData[i][column] = value;
+            break;
+        }
+    }
+
+}
+
+function datalistValidator(value,source) {
+    var obj = $("#" +source).find("option[value='" + value + "']");
+    if (obj != null && obj.length > 0) {
+        
+        return true
+    }
+     
+    return false;
+}
+
+function EG_Validate_changeData(obj, row, column,source) {
+    if (datalistValidator(obj.value, source))
+    {
+        obj.className = obj.className.replace(' EGDanger', '');
+    }
+    else {
+        obj.value = "";
+        obj.className = obj.className+ ' EGDanger'
+    }
+
+    EG_changeData(obj.value, row, column)
+}
+
+function EG_createTextBox(data, type, row, columnname) {
+
+    var a = row[columnname];
+    var b = row.SlNo;
+    var c = "'";
+    // debugger;
+    if (data == "" || data == null) {
+
+        return ('<input type="textbox" class="gridTextbox" value="" onblur="EG_changeData(this.value,' + b + ',' + c + columnname + c + ')" >  </input>');
+    } else {
+        return ('<input type="textbox" class="gridTextbox" value=' + data + ' onblur="EG_changeData(this.value,' + b + ',' + c + columnname + c + ')" >  </input>');
+    }
+
+
+}
+
+function EG_ComboSource(id, values) {
+    var options='';
+    for (var i = 0; i < values.length; i++)
+        options += '<option value="' + values[i] + '" />';
+   
+    document.getElementById(id).innerHTML = options;
+    //
+}
+
+function EG_createCombo(data, type, row, columnname,Source) {
+    var a = row[columnname];
+    var b = row.SlNo;
+    var c = "'";
+    return ('<input class="gridTextbox" list="' + Source + '" name="' + columnname + '" onblur="EG_Validate_changeData(this,' + b + ',' + c + columnname + c + ','+c+Source+c+')" >');
+
+ 
+}
+
+function EG_blankRow(count) {
+
+    var dataObj = [];
+    for (i = 0; i < count; i++) {
+
+        var tempObj = EG_TableDefn()
+        tempObj[EG_SlColumn] = EG_totalDetailRows + i + 1;
+        dataObj.push(tempObj);
+
+    }
+
+    EG_totalDetailRows = EG_totalDetailRows + count;
+    return dataObj;
+
+}
