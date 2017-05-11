9decb9134c9393eb341f11e0ac5eb49975ffc778
tvm-anija <anija.g@thrithvam.me>
2017-05-11 15:02:19 +0530

    Updated SalesReturn

--- SCManager.UserInterface/Models/SalesReturnViewModel.cs
+++ SCManager.UserInterface/Models/SalesReturnViewModel.cs
@@ -18,7 +18,7 @@ namespace SCManager.UserInterface.Models
         [Display(Name = "Reference No.")]
         public string RefNo { get; set; }
         public Guid? ItemID { get; set; }
-        [Display(Name = "Quantity (₹)")]
+        [Display(Name = "Quantity")]
         [Required(ErrorMessage = "Please enter Quantity")]
         [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Quantity must be a number")]
         public string Qty { get; set; }
