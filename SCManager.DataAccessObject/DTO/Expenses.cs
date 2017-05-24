88e8146c3725e0867620ad3c95ba17b8a41d6a4a
tvm-gibin <gibin.jacob@thrithvam.me>
2017-05-24 09:47:33 +0530

    New Module Expenses

---
+++ SCManager.DataAccessObject/DTO/Expenses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCManager.DataAccessObject.DTO
+{
+    public class Expenses
+    {
+
+        public string SCCode { get; set; }
+        public Guid ID { get; set; }
+        public string EntryNo { get; set; }
+        public string ExpenseTypeCode { get; set; }
+        public  string RefNo	{ get; set; }
+        public DateTime RefDate { get; set; }
+        public Guid EmpID { get; set; }
+        public string PaymentMode { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+
+        public LogDetails logDetails { get; set; }
+
+
+    }
+}
