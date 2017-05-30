fb541c44dfb159ac44902cd7c851c4c58a5087d5
tvm-gibin <gibin.jacob@thrithvam.me>
2017-05-30 10:40:36 +0530

    new files added ICRexpenses

---
+++ SCManager.BusinessService/Services/ICRExpensesBusiness.cs
@@ -0,0 +1,19 @@
+﻿using SCManager.BusinessService.Contracts;
+using SCManager.RepositoryServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCManager.BusinessService.Services
+{
+    public class ICRExpensesBusiness: IICRExpensesBusiness
+    {
+
+        private IICRExpensesRepository _iicrexpensesRepository;
+        public ICRExpensesBusiness(IICRExpensesRepository icrexpensesRepository)
+        {
+            _iicrexpensesRepository = icrexpensesRepository;
+        }
+    }
+}
---
+++ SCManager.DataAccessObject/DTO/ICRExpenses.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCManager.DataAccessObject.DTO
+{
+    public class ICRExpenses
+    {
+        public string SCCode { get; set; }
+        public Guid ID { get; set; }
+        public string EntryNo { get; set; }
+        public string ExpenseTypeCode { get; set; }
+        public string RefNo { get; set; }
+        public DateTime RefDate { get; set; }
+    //    public Guid EmpID { get; set; }
+        public string PaymentMode { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string EmpName { get; set; }
+        public string DateFormatted { get; set; }
+        public string ExpenseType { get; set; }
+        public decimal OutStandingPayment { get; set; }
+
+        public LogDetails logDetails { get; set; }
+    }
+}
