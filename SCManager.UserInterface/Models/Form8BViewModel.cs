9f68f4b4fac2d31d06dc32e6c2f1cf9d6522b364
suvaneeth <suvaneeth@gmail.com>
2017-06-01 12:01:57 +0300

    description

--- SCManager.UserInterface/Models/Form8BViewModel.cs
+++ SCManager.UserInterface/Models/Form8BViewModel.cs
@@ -107,6 +107,7 @@ namespace SCManager.UserInterface.Models
         public string Material { get; set; }
         public int? Quantity { get; set; }
         public string UOM { get; set; }
+        public string Description { get; set; }
         public decimal? Rate { get; set; }
         public decimal? BasicAmount { get; set; }
         public decimal? TradeDiscount { get; set; }
